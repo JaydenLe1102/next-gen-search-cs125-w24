1c38c1d5f8dc1a65a9446f51331c626618f5bf58
Jayden Le <61215492+JaydenLe1102@users.noreply.github.com>
2024-02-19 15:34:34 -0800

    New Changes: complete basic profile page (#7)
    Co-authored-by: Tracy <trinhva1@uci.edu>

---
+++ frontend/WellnessHub App/Profile/Profile.swift
@@ -0,0 +1,68 @@
+//
+//  profile.swift
+//  WellnessHub App
+//
+//  Created by BobaHolic on 2/17/24.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        VStack(alignment: .leading,spacing: 50, content: {
+            HStack(spacing: 105, content: {
+                VStack(content: {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 90, height: 90)
+                        .font(.title2)
+                        .padding(10)
+                        .background(Color.black.opacity(0.08))
+                        .cornerRadius(70.0)
+                    
+                    Text("Name")
+                })
+                
+                VStack(spacing: 40, content: {
+                    VStack(content: {
+                        Text("Weight")
+                        Text("178 lbs")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    })
+                    
+                    VStack(content: {
+                        Text("Goal")
+                        Text("125 lbs")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    })
+                })
+                
+                
+            })
+            VStack(alignment: .leading,spacing: 20, content: {
+                Text("Age:")
+                Text("Gender:")
+                Text("Height:")
+                Text("Activity Level:")
+                Text("Dietary Preferences:")
+                Text("Health Goal:")
+            })
+            
+            Button(action: {
+                            }) {
+                                Text("Edit Profile")
+                                    .frame(maxWidth: .infinity)
+                                    .foregroundColor(Color.blue)
+                                    .padding()
+                                    .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color.blue.opacity(0.2)))
+                            }
+        })
+        .padding(.horizontal, 20)
+
+    }
+}
+
+#Preview {
+    Profile()
+}
