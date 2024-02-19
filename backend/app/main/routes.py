a9544c883c207e18e6f1c6972cca0cf20114c5a8
hthere21 <114700489+hthere21@users.noreply.github.com>
2024-02-19 12:34:16 -0800

    First commit 2

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -4,14 +4,6 @@ import pyrebase
 
 
 config = {
-  'apiKey': "AIzaSyAgTCgwmHagAMa59FfdSnKAwchZg51OQCw",
-  'authDomain' : "cs125-project-b6ea5.firebaseapp.com",
-  'projectId' : "cs125-project-b6ea5",
-  'storageBucket' : "cs125-project-b6ea5.appspot.com",
-  'messagingSenderId' : "63446463144",
-  'appId' : "1:63446463144:web:5f3d8e158538109690489d",
-  'measurementId': "G-YKGZ6Q2317",
-   'databaseURL': '',
 }
 
 firebase = pyrebase.initialize_app(config)
